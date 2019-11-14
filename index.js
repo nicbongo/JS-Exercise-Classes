6dbb4bf1674d02b6688a210cb41bb69cdd620110
nicbongo <nicbongo@protonmail.com>
2019-11-14 16:06:12 -0400

    7, 49

--- index.js
+++ index.js
@@ -41,7 +41,22 @@ class Airplane {
 */
 
 class Person {
+  constructor (name, age) {
+    this.name = name,
+    this.age = age,
+    this.stomach = ""
+  }
 
+  eat(){
+
+  }
+  poop(){
+    
+  }
+
+  toString(){
+    return `${this.name}, ${this.age}`
+  }
 }
 
 /*
